56d01b435e1168cd5a6996c8b2610cbea57b9727
24MICAELA <mhuermor@myuax.com>
2024-12-16 07:00:54 +0100

    version 1.0

---
+++ tests/tests.h
@@ -0,0 +1,16 @@
+//
+// Created by Micae on 16/12/2024.
+//
+
+#ifndef TESTS_H
+#define TESTS_H
+
+
+
+class tests {
+
+};
+
+
+
+#endif //TESTS_H
---
+++ tiny-lisp/tinylisp.h
@@ -0,0 +1,16 @@
+//
+// Created by Micae on 16/12/2024.
+//
+
+#ifndef TINYLISP_H
+#define TINYLISP_H
+
+
+
+class tinylisp {
+
+};
+
+
+
+#endif //TINYLISP_H
